f485bef890b530c637eb5009ed9a45039571b9e8
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2007-05-10 17:18:32 +0000

    Encoding support per si CLI command different commands seem to use different encodings see http://code.google.com/p/ideamksplugin/issues/detail?id=5&can=2&q=

--- src/main/java/org/intellij/vcs/mks/MksConfiguration.java
+++ src/main/java/org/intellij/vcs/mks/MksConfiguration.java
@@ -6,12 +6,16 @@ import com.intellij.openapi.util.InvalidDataException;
 import com.intellij.openapi.util.JDOMExternalizable;
 import com.intellij.openapi.util.WriteExternalException;
 import org.jdom.Element;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MksConfiguration
 	implements JDOMExternalizable, ProjectComponent {
+	public static final String DEFAULT_ENCODING = Charset.defaultCharset().name();
 
 	public MksConfiguration() {
 		SERVER = "";
@@ -20,7 +24,8 @@ public class MksConfiguration
 		PASSWORD = "";
 		SANDBOX = "";
 		PROJECT = "";
-		SI_ENCODING = Charset.defaultCharset().name();
+		SI_ENCODINGS = new StringMap();
+		defaultEncoding = Charset.defaultCharset().name();
 	}
 
 	public void projectClosed() {
@@ -56,5 +61,52 @@ public class MksConfiguration
 	public String PASSWORD;
 	public String SANDBOX;
 	public String PROJECT;
-	public String SI_ENCODING;
+	public StringMap SI_ENCODINGS;
+	public String defaultEncoding;
+
+	public static class StringMap implements JDOMExternalizable {
+		@NonNls
+		private static final String LEN_STRING = "len";
+
+		private Map<String, String> map;
+
+		public StringMap() {
+			this.map = new HashMap<String, String>();
+		}
+
+		public Map<String, String> getMap() {
+			return map;
+		}
+
+		public void setMap(Map<String, String> map) {
+			this.map = map;
+		}
+
+		public void readExternal(Element element) throws InvalidDataException {
+			// Read in map size
+			int size = Integer.parseInt(element.getAttributeValue(LEN_STRING));
+
+			// Read in all elements in the proper order.
+			for (int index = 0; index < size; index++) {
+				this.map.put(element.getAttributeValue('k' + Integer.toString(index)),
+					element.getAttributeValue('v' + Integer.toString(index))
+				);
+			}
+		}
+
+
+		public void writeExternal(Element element) throws WriteExternalException {
+			// Write out map size
+			element.setAttribute(LEN_STRING, Integer.toString(this.map.size()));
+
+			// Write out all elements in the proper order.
+			int index = 0;
+			for (final Map.Entry<String, String> entry : this.map.entrySet()) {
+				element.setAttribute('k' + Integer.toString(index), entry.getKey());
+				element.setAttribute('v' + Integer.toString(index), entry.getValue());
+				index++;
+			}
+		}
+	}
+
 }
