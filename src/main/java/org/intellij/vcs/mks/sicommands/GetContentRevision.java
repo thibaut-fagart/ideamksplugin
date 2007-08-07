a96f8da712041a4918244fb0b6ee5f918c6bb6e7
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2007-08-07 15:44:18 +0000

    MksRevisionNumberRefactoring

--- src/main/java/org/intellij/vcs/mks/sicommands/GetContentRevision.java
+++ src/main/java/org/intellij/vcs/mks/sicommands/GetContentRevision.java
@@ -1,43 +1,42 @@
 package org.intellij.vcs.mks.sicommands;
 
-import com.intellij.openapi.vcs.VcsException;
-import org.intellij.vcs.mks.EncodingProvider;
-import org.intellij.vcs.mks.MksContentRevision;
-import org.jetbrains.annotations.NonNls;
-
 import java.io.IOException;
 import java.util.List;
+import org.intellij.vcs.mks.EncodingProvider;
+import org.intellij.vcs.mks.MksRevisionNumber;
+import org.jetbrains.annotations.NonNls;
+import com.intellij.openapi.vcs.VcsException;
 
 /**
  * @author Thibaut Fagart
  */
 public class GetContentRevision extends SiCLICommand {
-	@NonNls
-	public static final String COMMAND = "viewrevision";
+    @NonNls
+    public static final String COMMAND = "viewrevision";
 
-	public GetContentRevision(List<VcsException> errors, EncodingProvider encodingProvider, MksContentRevision mksContentRevision) {
-		super(errors, encodingProvider, COMMAND, "-r", mksContentRevision.getRevisionNumber().asString(), mksContentRevision.getFile().getPath());
+    public GetContentRevision(List<VcsException> errors, EncodingProvider encodingProvider, final MksRevisionNumber revisionNumber, final String path) {
+        super(errors, encodingProvider, COMMAND, "-r", revisionNumber.asString(), path);
 
-	}
+    }
 
-	@Override
-	public void execute() {
-		try {
-			executeCommand();
-		} catch (IOException e) {
-			//noinspection ThrowableInstanceNeverThrown
-			errors.add(new VcsException(e));
-		}
-	}
+    @Override
+    public void execute() {
+        try {
+            executeCommand();
+        } catch (IOException e) {
+            //noinspection ThrowableInstanceNeverThrown
+            errors.add(new VcsException(e));
+        }
+    }
 
-	public String getContent() throws VcsException {
-		if (foundError()) {
-			for (VcsException vcsException : errors.subList(previousErrorCount, errors.size())) {
-				LOGGER.error(vcsException);
-			}
-			throw new VcsException(errors.get(previousErrorCount));
-		} else {
-			return commandOutput;
-		}
-	}
+    public String getContent() throws VcsException {
+        if (foundError()) {
+            for (VcsException vcsException : errors.subList(previousErrorCount, errors.size())) {
+                LOGGER.error(vcsException);
+            }
+            throw new VcsException(errors.get(previousErrorCount));
+        } else {
+            return commandOutput;
+        }
+    }
 }
