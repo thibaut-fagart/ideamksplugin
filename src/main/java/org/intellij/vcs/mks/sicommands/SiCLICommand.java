6186053138a6c868c52141dbc06721990c658553
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2007-08-07 15:37:44 +0000

    always add --batch parameter to prevent si process getting stuck on user input. Add a setWorkingDir method to allow easy setup of servers/pass etc based on sandbox

--- src/main/java/org/intellij/vcs/mks/sicommands/SiCLICommand.java
+++ src/main/java/org/intellij/vcs/mks/sicommands/SiCLICommand.java
@@ -1,58 +1,72 @@
 package org.intellij.vcs.mks.sicommands;
 
-import com.intellij.openapi.vcs.VcsException;
-import org.intellij.vcs.mks.AbstractMKSCommand;
-import org.intellij.vcs.mks.EncodingProvider;
-
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.util.List;
+import org.intellij.vcs.mks.AbstractMKSCommand;
+import org.intellij.vcs.mks.EncodingProvider;
+import com.intellij.openapi.vcs.VcsException;
 
 /**
  * @author Thibaut Fagart
  */
 public abstract class SiCLICommand extends AbstractMKSCommand {
-	protected final EncodingProvider encodingProvider;
-	private String command;
-	private String[] args;
-	protected String commandOutput;
+    protected final EncodingProvider encodingProvider;
+    private String command;
+    private String[] args;
+    protected String commandOutput;
+    private File workingDir;
 
-	public SiCLICommand(List<VcsException> errors, EncodingProvider encodingProvider, String command, String... args) {
-		super(errors);
-		this.encodingProvider = encodingProvider;
-		this.command = command;
-		this.args = args;
-	}
+    public SiCLICommand(List<VcsException> errors, EncodingProvider encodingProvider, String command, String... args) {
+        super(errors);
+        this.encodingProvider = encodingProvider;
+        this.command = command;
+        this.args = args;
+    }
 
-	protected void executeCommand() throws IOException {
-		String[] processArgs = new String[args.length + 2];
-		processArgs[0] = "si";
-		processArgs[1] = command;
-		System.arraycopy(args, 0, processArgs, 2, args.length);
-		ProcessBuilder builder = new ProcessBuilder(processArgs);
-		LOGGER.info("executing " + builder.command());
-		builder.redirectErrorStream(true);
-		Process process = builder.start();
-		InputStream is = process.getInputStream();
-		InputStreamReader reader = new InputStreamReader(is, encodingProvider.getMksSiEncoding(command));
-		StringWriter sw;
-		try {
-			char[] buffer = new char[512];
-			int readChars;
-			sw = new StringWriter();
-			while ((readChars = reader.read(buffer)) != -1) {
-				sw.write(new String(buffer, 0, readChars));
-			}
-		} finally {
-			reader.close();
-		}
-		commandOutput = sw.toString();
+    public void setWorkingDir(File aDir) {
+        workingDir = aDir;
+    }
 
-	}
+    protected String executeCommand() throws IOException {
+        String[] processArgs = new String[args.length + 3];
+        processArgs[0] = "si";
+        processArgs[1] = command;
+        processArgs[2] = "--batch";
+        System.arraycopy(args, 0, processArgs, 3, args.length);
+        ProcessBuilder builder = new ProcessBuilder(processArgs);
+        if (workingDir != null) {
+            builder.directory(workingDir);
+        }
+        StringBuffer buf = new StringBuffer();
+        for (String s : builder.command()) {
+            buf.append(s);
+            buf.append(" ");
+        }
+        LOGGER.info("executing " + buf.toString());
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+        InputStream is = process.getInputStream();
+        InputStreamReader reader = new InputStreamReader(is, encodingProvider.getMksSiEncoding(command));
+        StringWriter sw;
+        try {
+            char[] buffer = new char[512];
+            int readChars;
+            sw = new StringWriter();
+            while ((readChars = reader.read(buffer)) != -1) {
+                sw.write(new String(buffer, 0, readChars));
+            }
+        } finally {
+            reader.close();
+        }
+        commandOutput = sw.toString();
+        return buf.toString();
+    }
 
-	protected boolean shoudIgnore(String line) {
-		return line.startsWith("Reconnecting") || line.startsWith("Connecting");
-	}
+    protected boolean shoudIgnore(String line) {
+        return line.startsWith("Reconnecting") || line.startsWith("Connecting");
+    }
 }
