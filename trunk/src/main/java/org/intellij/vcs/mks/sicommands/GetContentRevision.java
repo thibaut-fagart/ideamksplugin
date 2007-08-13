66239a6bfd4cff9ecd48603541738f63a1154d50
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2007-08-13 12:56:09 +0000

    profiling logging for SiCliCommands

--- trunk/src/main/java/org/intellij/vcs/mks/sicommands/GetContentRevision.java
+++ trunk/src/main/java/org/intellij/vcs/mks/sicommands/GetContentRevision.java
@@ -13,10 +13,13 @@ import com.intellij.openapi.vcs.VcsException;
 public class GetContentRevision extends SiCLICommand {
     @NonNls
     public static final String COMMAND = "viewrevision";
+    private final String member;
+    private final MksRevisionNumber revisionNumber;
 
     public GetContentRevision(List<VcsException> errors, EncodingProvider encodingProvider, final MksRevisionNumber revisionNumber, final String path) {
         super(errors, encodingProvider, COMMAND, "-r", revisionNumber.asString(), path);
-
+        this.member = path;
+        this.revisionNumber = revisionNumber;
     }
 
     @Override
@@ -38,5 +41,9 @@ public class GetContentRevision extends SiCLICommand {
         } else {
             return commandOutput;
         }
+    }
+    @Override
+    public String toString() {
+        return "GetContentRevision["+member+","+revisionNumber.asString()+"]";
     }
 }
