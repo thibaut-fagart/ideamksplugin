f3735bfb270c8a23742224cf7e99d048359d9a1d
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2007-10-04 17:01:34 +0000

    support dropped files when synchronizing sandboxes

--- branches/feature_mks_listener/src/main/java/org/intellij/vcs/mks/sicommands/ViewSandboxMissingCommand.java
+++ branches/feature_mks_listener/src/main/java/org/intellij/vcs/mks/sicommands/ViewSandboxMissingCommand.java
@@ -1,10 +1,10 @@
 package org.intellij.vcs.mks.sicommands;
 
-import java.util.List;
-import org.intellij.vcs.mks.EncodingProvider;
-import org.intellij.vcs.mks.MksRevisionNumber;
-import org.intellij.vcs.mks.model.MksMemberState;
 import com.intellij.openapi.vcs.VcsException;
+import org.intellij.vcs.mks.EncodingProvider;
+import org.intellij.vcs.mks.model.MksMemberState;
+
+import java.util.List;
 
 /**
  * Obtains member revision, working revision, checkedout state (won't see
@@ -16,13 +16,14 @@ public class ViewSandboxMissingCommand extends AbstractViewSandboxCommand {
 
 
 	public ViewSandboxMissingCommand(final List<VcsException> errors, final EncodingProvider encodingProvider,
-	                                 final String username, final String sandboxPath) {
+									 final String username, final String sandboxPath) {
 		super(errors, encodingProvider, username, sandboxPath,/* "--filter=changed",*/"--filter=changed:missing");
 	}
 
 	@Override
 	protected MksMemberState createState(final String workingRev, final String memberRev, final String workingCpid, final String locker, final String lockedSandbox, final String type, final String deferred) throws VcsException {
-		return new MksMemberState(createRevision(workingRev), new MksRevisionNumber(memberRev), workingCpid, MksMemberState.Status.MISSISNG);
+		return new MksMemberState(createRevision(workingRev), createRevision(memberRev), workingCpid,
+				(DROPPED_TYPE.equals(type) ? MksMemberState.Status.DROPPED : MksMemberState.Status.MISSISNG));
 	}
 
 }
