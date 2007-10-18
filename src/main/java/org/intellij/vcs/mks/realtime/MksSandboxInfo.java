ff117f528281b9a23f933dca1063264b65177dd8
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2007-10-18 12:41:09 +0000

    get away from TriclopsSiSandbox (cont)

--- src/main/java/org/intellij/vcs/mks/realtime/MksSandboxInfo.java
+++ src/main/java/org/intellij/vcs/mks/realtime/MksSandboxInfo.java
@@ -1,7 +1,10 @@
 package org.intellij.vcs.mks.realtime;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.vfs.VirtualFile;
+import mks.integrations.common.TriclopsException;
 import mks.integrations.common.TriclopsSiSandbox;
+import org.intellij.vcs.mks.MKSHelper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -9,12 +12,26 @@ import org.jetbrains.annotations.Nullable;
  * @author Thibaut Fagart
  */
 public final class MksSandboxInfo implements Comparable<MksSandboxInfo> {
+	private final Logger LOGGER = Logger.getInstance(getClass().getName());
 	public final String sandboxPath;
 	public final String hostAndPort;
 	public final String mksProject;
 	public final String devPath;
 	final VirtualFile sandboxPjFile;
-	TriclopsSiSandbox siSandbox;
+	int retries = 0;
+	private TriclopsSiSandbox siSandbox;
+
+	@Deprecated
+	public synchronized TriclopsSiSandbox getSiSandbox() {
+		if (siSandbox == null) {
+			try {
+				siSandbox = MKSHelper.createSandbox(sandboxPath);
+			} catch (TriclopsException e) {
+				LOGGER.error("error fetching MKS native sandbox for " + sandboxPath);
+			}
+		}
+		return siSandbox;
+	}
 
 	/**
 	 * @param sandboxPath
@@ -65,11 +82,5 @@ public final class MksSandboxInfo implements Comparable<MksSandboxInfo> {
 	@Override
 	public String toString() {
 		return "MksSandbox[" + sandboxPath + "," + hostAndPort + "]";
-	}
-
-
-	@Deprecated
-	public TriclopsSiSandbox getSiSandbox() {
-		return siSandbox;
 	}
 }
