643a25707936439b0c992cfec17b604111d1413b
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2007-10-19 10:37:51 +0000

    CLI version of ViewSandbox

---
+++ branches/feature_mks_listener/src/main/java/org/intellij/vcs/mks/actions/cli/ViewSandboxCLICommand.java
@@ -0,0 +1,48 @@
+package org.intellij.vcs.mks.actions.cli;
+
+import com.intellij.openapi.vcs.AbstractVcs;
+import com.intellij.openapi.vcs.VcsException;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.intellij.vcs.mks.EncodingProvider;
+import org.intellij.vcs.mks.MksVcs;
+import org.intellij.vcs.mks.actions.MksCommand;
+import org.intellij.vcs.mks.realtime.MksSandboxInfo;
+import org.intellij.vcs.mks.sicommands.SiCLICommand;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ViewSandboxCLICommand implements MksCommand {
+	static class ViewSandboxCommand extends SiCLICommand {
+		private final String sandboxPath;
+
+		ViewSandboxCommand(@NotNull List<VcsException> errors, @NotNull EncodingProvider encodingProvider, MksSandboxInfo sandbox) {
+			super(errors, encodingProvider, "viewsandbox", "--gui", "--sandbox=" + sandbox.sandboxPath);
+			sandboxPath = sandbox.sandboxPath;
+		}
+
+		public void execute() {
+			try {
+				executeCommand();
+			} catch (IOException e) {
+				LOGGER.error("error opening sandbox " + sandboxPath + " in MKS client", e);
+			}
+		}
+	}
+
+	public void executeCommand(@NotNull MksVcs mksVcs, @NotNull List<VcsException> exceptions, @NotNull VirtualFile[] affectedFiles) throws VcsException {
+		Map<MksSandboxInfo, ArrayList<VirtualFile>> map = mksVcs.dispatchBySandbox(affectedFiles);
+
+		for (MksSandboxInfo sandbox : map.keySet()) {
+			new ViewSandboxCommand(exceptions, mksVcs, sandbox).execute();
+		}
+	}
+
+	@NotNull
+	public String getActionName(@NotNull AbstractVcs vcs) {
+		return "View Sandbox";
+	}
+}
