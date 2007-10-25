bb4cdbc7e5f33e9b35073f86873a09fc264152d6
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2007-10-25 09:47:51 +0000

    introduce MksRevisionNumber factory method

--- branches/feature_mks_listener/src/main/java/org/intellij/vcs/mks/sicommands/ViewSandboxWithoutChangesCommand.java
+++ branches/feature_mks_listener/src/main/java/org/intellij/vcs/mks/sicommands/ViewSandboxWithoutChangesCommand.java
@@ -3,6 +3,7 @@ package org.intellij.vcs.mks.sicommands;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.history.VcsRevisionNumber;
 import org.intellij.vcs.mks.EncodingProvider;
+import org.intellij.vcs.mks.MksRevisionNumber;
 import org.intellij.vcs.mks.model.MksMemberState;
 
 import java.util.List;
@@ -16,24 +17,24 @@ import java.util.List;
 public class ViewSandboxWithoutChangesCommand extends AbstractViewSandboxCommand {
 
 
-	public ViewSandboxWithoutChangesCommand(final List<VcsException> errors, final EncodingProvider encodingProvider, final String sandboxPath) {
-		super(errors, encodingProvider, sandboxPath, "--filter=!changed", "--filter=!deferred");
-	}
+    public ViewSandboxWithoutChangesCommand(final List<VcsException> errors, final EncodingProvider encodingProvider, final String sandboxPath) {
+        super(errors, encodingProvider, sandboxPath, "--filter=!changed", "--filter=!deferred");
+    }
 
-	@Override
-	protected MksMemberState createState(final String workingRev, final String memberRev, final String workingCpid,
-										 final String locker, final String lockedSandbox, final String type, final String deferred) throws VcsException {
-		VcsRevisionNumber workingRevision = createRevision(workingRev);
-		VcsRevisionNumber memberRevision = createRevision(memberRev);
-		if (!"archived".equals(type)) {
-			LOGGER.warn("exepecting only non changed members " + type);
+    @Override
+    protected MksMemberState createState(final String workingRev, final String memberRev, final String workingCpid,
+                                         final String locker, final String lockedSandbox, final String type, final String deferred) throws VcsException {
+        VcsRevisionNumber workingRevision = MksRevisionNumber.createRevision(workingRev);
+        VcsRevisionNumber memberRevision = MksRevisionNumber.createRevision(memberRev);
+        if (!"archived".equals(type)) {
+            LOGGER.warn("exepecting only non changed members " + type);
 //			return new MksMemberState(workingRevision, memberRevision, workingCpid, MksMemberState.Status.DROPPED);
 //		} else if (memberRevision != null && workingRevision == null) {
 //			return new MksMemberState(workingRevision, memberRevision, workingCpid, MksMemberState.Status.MISSISNG);
-			throw new VcsException("exepecting only non changed members");
-		} else {
-			return new MksMemberState(workingRevision, memberRevision, workingCpid, MksMemberState.Status.NOT_CHANGED);
-		}
-	}
+            throw new VcsException("exepecting only non changed members");
+        } else {
+            return new MksMemberState(workingRevision, memberRevision, workingCpid, MksMemberState.Status.NOT_CHANGED);
+        }
+    }
 
 }
