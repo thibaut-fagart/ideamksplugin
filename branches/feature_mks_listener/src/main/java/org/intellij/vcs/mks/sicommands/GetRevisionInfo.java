8e69bace4643aca4d4e2e4136af8d4e5b6a035dc
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2007-10-30 16:17:21 +0000

    better user feedback (logs, exceptions ...)

--- branches/feature_mks_listener/src/main/java/org/intellij/vcs/mks/sicommands/GetRevisionInfo.java
+++ branches/feature_mks_listener/src/main/java/org/intellij/vcs/mks/sicommands/GetRevisionInfo.java
@@ -15,76 +15,75 @@ import java.util.regex.Pattern;
  * @author Thibaut Fagart
  */
 public class GetRevisionInfo extends SiCLICommand {
-    public static final String COMMAND = "rlog";
-    public static final String patternString = "([\\d\\.]*)(?:\\s+)([\\d\\.]*)(?:\\s+)([\\d\\.]*)"; // 1 : branchtip, 2 : member, 3 : working
-    private final Pattern pattern = Pattern.compile(patternString);
-    private VcsRevisionNumber branchTip;
-    private VcsRevisionNumber memberRev;
-    private VcsRevisionNumber workingRev;
-    private final String memberPath;
+	public static final String COMMAND = "rlog";
 
-    public GetRevisionInfo(List<VcsException> errors, EncodingProvider encodingProvider, String memberPath, File directory) {
-        super(errors, encodingProvider, COMMAND, "--noheaderformat", "--notrailerformat", "--fields=branchtiprev,memberrev,workingrev", memberPath);
-        this.memberPath = memberPath;
-        setWorkingDir(directory);
-    }
+	public static final String patternString = revisionPattern + "(?:\\s+)" + revisionPattern + "(?:\\s+)" + revisionPattern; // 1 : branchtip, 2 : member, 3 : working
+	private final Pattern pattern = Pattern.compile(patternString);
+	private VcsRevisionNumber branchTip;
+	private VcsRevisionNumber memberRev;
+	private VcsRevisionNumber workingRev;
+	private final String memberPath;
 
-    @Override
-    public void execute() {
-        try {
-            executeCommand();
-            String[] lines = commandOutput.split("\n");
-            int start = 0;
-            while (shoudIgnore(lines[start])) {
-                // skipping connecting/reconnecting lines
-                start++;
-            }
-            // only top line seems to be interesting, we're not interested in the history
-            String line = lines[start];
-            Matcher matcher = pattern.matcher(line);
-            if (matcher.matches()) {
-                branchTip = MksRevisionNumber.createRevision(matcher.group(1));
-                memberRev = MksRevisionNumber.createRevision(matcher.group(2));
-                workingRev = MksRevisionNumber.createRevision(matcher.group(3));
-            } else
-            if (line.contains("is not a current or destined or pending member")) {
-                LOGGER.warn(memberPath + " is not a mks member (any more?)");
-            } else {
-                LOGGER.error("unexpected command output {" + line + "}, expected (" + patternString + ")");
-            }
-        } catch (IOException e) {
-            //noinspection ThrowableInstanceNeverThrown
-            errors.add(new VcsException(e));
-        } catch (VcsException e) {
-            errors.add(e);
-        }
-    }
+	public GetRevisionInfo(List<VcsException> errors, EncodingProvider encodingProvider, String memberPath, File directory) {
+		super(errors, encodingProvider, COMMAND, "--noheaderformat", "--notrailerformat", "--fields=branchtiprev,memberrev,workingrev", memberPath);
+		this.memberPath = memberPath;
+		setWorkingDir(directory);
+	}
 
-    /**
-     * beware : branch tip is the FILE branch, not the development path
-     *
-     * @return revision number
-     */
-    public VcsRevisionNumber getBranchTip() {
-        return branchTip;
-    }
+	@Override
+	public void execute() {
+		try {
+			executeCommand();
+			String[] lines = commandOutput.split("\n");
+			int start = 0;
+			while (shouldIgnore(lines[start])) {
+				// skipping connecting/reconnecting lines
+				start++;
+			}
+			// only top line seems to be interesting, we're not interested in the history
+			String line = lines[start];
+			Matcher matcher = pattern.matcher(line);
+			if (matcher.matches()) {
+				branchTip = MksRevisionNumber.createRevision(matcher.group(1));
+				memberRev = MksRevisionNumber.createRevision(matcher.group(2));
+				workingRev = MksRevisionNumber.createRevision(matcher.group(3));
+			} else if (line.contains("is not a current or destined or pending member")) {
+				LOGGER.warn(this + " not a mks member (any more?) " + memberPath);
+			} else {
+				LOGGER.error(this + " unexpected command output {" + line + "}, expected (" + patternString + ")");
+			}
+		} catch (IOException e) {
+			//noinspection ThrowableInstanceNeverThrown
+			errors.add(new VcsException(e));
+		} catch (VcsException e) {
+			errors.add(e);
+		}
+	}
 
-    /**
-     * member rev ON CURRENT THE DEVELOPMENT PATH
-     *
-     * @return revision number
-     */
-    public VcsRevisionNumber getMemberRev() {
-        return memberRev;
-    }
+	/**
+	 * beware : branch tip is the FILE branch, not the development path
+	 *
+	 * @return revision number
+	 */
+	public VcsRevisionNumber getBranchTip() {
+		return branchTip;
+	}
 
-    public VcsRevisionNumber getWorkingRev() {
-        return workingRev;
-    }
+	/**
+	 * member rev ON CURRENT THE DEVELOPMENT PATH
+	 *
+	 * @return revision number
+	 */
+	public VcsRevisionNumber getMemberRev() {
+		return memberRev;
+	}
 
-    @Override
-    public String toString() {
-        return "GetRevisionInfo[" + memberPath + "]";
-    }
+	public VcsRevisionNumber getWorkingRev() {
+		return workingRev;
+	}
 
+	@Override
+	public String toString() {
+		return "GetRevisionInfo[" + memberPath + "]";
+	}
 }
