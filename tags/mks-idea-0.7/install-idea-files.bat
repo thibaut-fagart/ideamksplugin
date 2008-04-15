17fd50056f6bd34596e71d020671869f949c0942
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2008-04-15 14:16:41 +0000

    Removed file/folder

--- tags/mks-idea-0.7/install-idea-files.bat
+++
@@ -1,9 +0,0 @@
-rem complete your IDEA Home folder here
-set IDEA_HOME=H:\developpement\IntelliJ IDEA 7.0M2
-set IDEA_VERSION=7.0.7126
-set MKS_VERSION=8.3.2392
-
-call mvn install:install-file -DgroupId=com.intellij.idea -Dpackaging=jar -Dfile="%IDEA_HOME%\lib\openapi.jar" -DartifactId=openapi -Dversion=%IDEA_VERSION%
-call mvn install:install-file -DgroupId=com.intellij.idea -Dpackaging=jar -Dfile="%IDEA_HOME%\lib\annotations.jar" -DartifactId=annotations -Dversion=%IDEA_VERSION%
-call mvn install:install-file -DgroupId=com.intellij.idea -Dpackaging=jar -Dfile="%IDEA_HOME%\lib\extensions.jar" -DartifactId=extensions -Dversion=%IDEA_VERSION%
-call mvn install:install-file -DgroupId=com.mks -Dpackaging=jar -Dfile="lib\%MKS_VERSION%\mkscmapi.jar" -DartifactId=mkscmapi -Dversion=%MKS_VERSION%
