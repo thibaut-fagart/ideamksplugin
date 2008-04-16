9868f074818dc55a78aa9fb30f48431aa1908432
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2008-04-16 12:22:29 +0000

    Copied remotely

---
+++ branches/IDEA 8.x/mks-idea-0.7/src/main/java/org/intellij/vcs/mks/actions/triclops/AddMemberCommand.java
@@ -0,0 +1,21 @@
+package org.intellij.vcs.mks.actions.triclops;
+
+import org.intellij.vcs.mks.MKSHelper;
+import org.intellij.vcs.mks.MksBundle;
+import org.intellij.vcs.mks.actions.MksCommand;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.vcs.AbstractVcs;
+import mks.integrations.common.TriclopsException;
+import mks.integrations.common.TriclopsSiMembers;
+
+public class AddMemberCommand extends AbstractMultipleTargetTriclopsCommand implements MksCommand {
+	protected void perform(@NotNull TriclopsSiMembers siMembers) throws TriclopsException {
+		MKSHelper.addMembers(siMembers, 0);
+	}
+
+	@NotNull
+	public String getActionName(@NotNull AbstractVcs vcs) {
+		return MksBundle.message("action.addmembers");
+	}
+
+}
