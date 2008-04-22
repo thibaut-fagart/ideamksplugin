072b5b6fbc85e43f78755e39f616eb13bb8130d2
thibaut.fagart@gmail.com <thibaut.fagart@gmail.com@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2008-04-22 22:05:43 +0000

    issue 41 : externalize CLI date pattern

--- src/main/java/org/intellij/vcs/mks/sicommands/ViewSandboxWithoutChangesCommand.java
+++ src/main/java/org/intellij/vcs/mks/sicommands/ViewSandboxWithoutChangesCommand.java
@@ -1,10 +1,11 @@
 package org.intellij.vcs.mks.sicommands;
 
-import java.util.List;
-import org.intellij.vcs.mks.EncodingProvider;
-import org.intellij.vcs.mks.model.MksMemberState;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.history.VcsRevisionNumber;
+import org.intellij.vcs.mks.MksCLIConfiguration;
+import org.intellij.vcs.mks.model.MksMemberState;
+
+import java.util.List;
 
 /**
  * Obtains members that HAVE NOT BEEN CHANGED LOCALLY.
@@ -15,28 +16,28 @@ import com.intellij.openapi.vcs.history.VcsRevisionNumber;
 public class ViewSandboxWithoutChangesCommand extends AbstractViewSandboxCommand {
 
 
-	public ViewSandboxWithoutChangesCommand(final List<VcsException> errors, final EncodingProvider encodingProvider, final String sandboxPath) {
-		super(errors, encodingProvider, sandboxPath, "--filter=!changed", "--filter=!deferred");
-	}
+    public ViewSandboxWithoutChangesCommand(final List<VcsException> errors, final MksCLIConfiguration mksCLIConfiguration, final String sandboxPath) {
+        super(errors, mksCLIConfiguration, sandboxPath, "--filter=!changed", "--filter=!deferred");
+    }
 
-	@Override
-	protected MksMemberState createState(final String workingRev, final String memberRev, final String workingCpid,
-										 final String locker, final String lockedSandbox, final String type, final String deferred) throws VcsException {
-		VcsRevisionNumber workingRevision = createRevision(workingRev);
-		VcsRevisionNumber memberRevision = createRevision(memberRev);
-		if (!"archived".equals(type)) {
-			LOGGER.warn("exepecting only non changed members " + type);
+    @Override
+    protected MksMemberState createState(final String workingRev, final String memberRev, final String workingCpid,
+                                         final String locker, final String lockedSandbox, final String type, final String deferred) throws VcsException {
+        VcsRevisionNumber workingRevision = createRevision(workingRev);
+        VcsRevisionNumber memberRevision = createRevision(memberRev);
+        if (!"archived".equals(type)) {
+            LOGGER.warn("exepecting only non changed members " + type);
 //			return new MksMemberState(workingRevision, memberRevision, workingCpid, MksMemberState.Status.DROPPED);
 //		} else if (memberRevision != null && workingRevision == null) {
 //			return new MksMemberState(workingRevision, memberRevision, workingCpid, MksMemberState.Status.MISSISNG);
-			throw new VcsException("exepecting only non changed members");
-		} else {
-			return new MksMemberState(workingRevision, memberRevision, workingCpid, MksMemberState.Status.NOT_CHANGED);
-		}
-	}
+            throw new VcsException("exepecting only non changed members");
+        } else {
+            return new MksMemberState(workingRevision, memberRevision, workingCpid, MksMemberState.Status.NOT_CHANGED);
+        }
+    }
 
-	@Override
-	protected boolean isRelevant(String type) {
-		return super.isRelevant(type) && !isDropped(type);
-	}
+    @Override
+    protected boolean isRelevant(String type) {
+        return super.isRelevant(type) && !isDropped(type);
+    }
 }
