77883d3c1f290c1aca4e3e0e870941b020870bbf
thibaut.fagart@gmail.com <thibaut.fagart@gmail.com@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2008-05-20 22:54:46 +0000

    issue 49 : change how sandbox list changes are picked up

--- src/main/java/org/intellij/vcs/mks/realtime/SandboxListSynchronizerImpl.java
+++ src/main/java/org/intellij/vcs/mks/realtime/SandboxListSynchronizerImpl.java
@@ -3,227 +3,203 @@ package org.intellij.vcs.mks.realtime;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.vcs.VcsException;
 import org.intellij.vcs.mks.MKSHelper;
 import org.intellij.vcs.mks.MksConfiguration;
 import org.intellij.vcs.mks.sicommands.ListSandboxes;
+import org.intellij.vcs.mks.sicommands.SandboxesCommand;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
+ * starts a si sandboxes --persist and listens to updates but doesn't actually use the output.
+ * Whenever a new update is detected, issues a si sandboxes (without --persist), compares the output to
+ * the previous state and issues events accordingly to any registered listeners.
+ * <p/>
+ * A listener being registered while comparison is in progress should get the new list
+ *
  * @author Thibaut Fagart
  */
 public class SandboxListSynchronizerImpl extends AbstractMKSSynchronizer
-		implements ApplicationComponent, SandboxListSynchronizer {
-	//	private static final String LINE_SEPARATOR = " -> ";
-	// $sandbox$ -> $project$[$sandboxtype$:$projectVersionOrDevPath$] ($server$:$port$)
-	private static final String patternString = "(.+) -> ([^\\[]+)(?:\\[([^:]+):([^:]+)\\])? \\((.+)\\)";
-	private final ArrayList<SandboxListListener> listeners = new ArrayList<SandboxListListener>();
-	private final Pattern pattern;
-	private static final int SANDBOX_PATH_GROUP_IDX = 1;
-	private static final int PROJECT_PATH_GROUP_IDX = 2;
-	private static final int PROJECT_TYPE_GROUP_IDX = 3;
-	private static final int PROJECT_VERSION_GROUP_IDX = 4;
-	private static final int SERVER_GROUP_IDX = 5;
-	private static final String MKS_PROJECT_PJ = "project.pj";
-
-	private SandboxInfo currentTopSandbox = null;
-
-	private final ArrayList<SandboxInfo> currentBatch = new ArrayList<SandboxInfo>();
-	private final ReentrantLock sandboxCacheLock = new ReentrantLock();
-
-	public SandboxListSynchronizerImpl() {
-		this(ApplicationManager.getApplication().getComponent(MksConfiguration.class));
-	}
-
-	protected SandboxListSynchronizerImpl(MksConfiguration config) {
-		super(ListSandboxes.COMMAND, config, "--displaySubs");
-		pattern = Pattern.compile(patternString);
-	}
-
-	public void addListener(@NotNull SandboxListListener listener) {
-		if (this.listeners.contains(listener)) {
-			return;
-		}
-		sandboxCacheLock.lock();
-		try {
-			this.listeners.add(listener);
-			for (SandboxInfo sandbox : currentBatch) {
-				listener.addSandboxPath(sandbox.sandboxPath, sandbox.serverHostAndPort, sandbox.projectPath,
-						sandbox.projectVersion, sandbox.subSandbox);
-			}
-		} finally {
-			sandboxCacheLock.unlock();
-		}
-	}
-
-	public void removeListener(@NotNull SandboxListListener sandboxListListener) {
-		this.listeners.remove(sandboxListListener);
-	}
+        implements ApplicationComponent, SandboxListSynchronizer {
+    private final ArrayList<SandboxListListener> listeners = new ArrayList<SandboxListListener>();
+    //	private static final String LINE_SEPARATOR = " -> ";
+    private static final String MKS_PROJECT_PJ = "project.pj";
 
 
-	@NonNls
-	@NotNull
-	public String getComponentName() {
-		return "MKS.sandboxListSaynchronizer";
-	}
+    private ArrayList<SandboxesCommand.SandboxInfo> currentList = new ArrayList<SandboxesCommand.SandboxInfo>();
+    private final ReentrantLock sandboxCacheLock = new ReentrantLock();
 
-	public void initComponent() {
-		MKSHelper.startClient();
-		addIgnoredFiles();
-		start();
-	}
+    public SandboxListSynchronizerImpl() {
+        this(ApplicationManager.getApplication().getComponent(MksConfiguration.class));
+    }
 
-	public void disposeComponent() {
-		stop();
-	}
+    protected SandboxListSynchronizerImpl(MksConfiguration config) {
+        super(ListSandboxes.COMMAND, config, "--displaySubs");
+    }
 
-	private static void addIgnoredFiles() {
-		String patterns = FileTypeManager.getInstance().getIgnoredFilesList();
+    public void addListener(@NotNull SandboxListListener listener) {
+        if (this.listeners.contains(listener)) {
+            return;
+        }
+        sandboxCacheLock.lock();
+        try {
+            this.listeners.add(listener);
+            for (SandboxesCommand.SandboxInfo sandbox : currentList) {
+                listener.addSandboxPath(sandbox.sandboxPath, sandbox.serverHostAndPort, sandbox.projectPath,
+                        sandbox.projectVersion, sandbox.subSandbox);
+            }
+        } finally {
+            sandboxCacheLock.unlock();
+        }
+    }
 
-		StringBuffer newPattern = new StringBuffer(patterns);
-		if (patterns.indexOf(MKS_PROJECT_PJ) == -1) {
-			newPattern.append((newPattern.charAt(newPattern.length() - 1) == ';') ? "" : ";").append(MKS_PROJECT_PJ);
-		}
+    public void removeListener(@NotNull SandboxListListener sandboxListListener) {
+        this.listeners.remove(sandboxListListener);
+    }
 
-		final String newPatternString = newPattern.toString();
-		if (!newPatternString.equals(patterns)) {
-			ApplicationManager.getApplication().runWriteAction(new Runnable() {
-				public void run() {
-					FileTypeManager.getInstance().setIgnoredFilesList(newPatternString);
-				}
-			}
-			);
-		}
-	}
 
-	@Override
-	protected void handleLine(String line) {
-		try {
-			if (shoudIgnore(line)) {
-				return;
-			}
-			if (line.startsWith("-----")) {
-				// detection of a new update
-				LOGGER.debug("update notification : " + line);
-				fireSandboxReset();
-			} else {
+    @NonNls
+    @NotNull
+    public String getComponentName() {
+        return "MKS.sandboxListSaynchronizer";
+    }
 
-				Matcher matcher = pattern.matcher(line);
-//				String[] parts = line.split(LINE_SEPARATOR);
-				if (!matcher.matches()) {
-					LOGGER.error(
-							"unexpected command output {" + line + "}, expected something matching " + patternString,
-							"");
-					// ignoring line
-				} else {
-					String sandboxPath = matcher.group(SANDBOX_PATH_GROUP_IDX);
-					String projectPath = matcher.group(PROJECT_PATH_GROUP_IDX);
-					String projectType = matcher.group(PROJECT_TYPE_GROUP_IDX);
-					String projectVersion = matcher.group(PROJECT_VERSION_GROUP_IDX);
-					String serverHostAndPort = matcher.group(SERVER_GROUP_IDX);
-					final SandboxInfo info =
-							resolveSandbox(sandboxPath, serverHostAndPort, projectPath, projectVersion);
-					fireSandboxAdded(info);
-				}
-			}
-		} catch (Exception e) {
-			LOGGER.error("error parsing mks synchronizer output [" + line + "], skipping that line  because : " +
-					e.getMessage(), e);
-		}
-	}
+    public void initComponent() {
+        MKSHelper.startClient();
+        addIgnoredFiles();
+        start();
+    }
 
-	private SandboxInfo resolveSandbox(String sandboxPath, String serverHostAndPort, String projectPath,
-									   String projectVersion) {
-		boolean isSubSandbox = isSubSandbox(projectPath);
-		SandboxInfo info;
-		if (isSubSandbox) {
-			if (currentTopSandbox == null) {
-				throw new IllegalStateException("encountering a subsandbox without its containing sandbox");
-			}
-			info = new SandboxInfo(currentTopSandbox, sandboxPath, projectPath, projectVersion);
-		} else {
-			if (projectPath.indexOf('/') < 0) {
-				throw new IllegalStateException("projectPath [" + projectPath + "] does not contain /");
-			}
-			info = new SandboxInfo(sandboxPath, serverHostAndPort, projectPath, projectVersion);
-			currentTopSandbox = info;
-		}
-		return info;
-	}
+    public void disposeComponent() {
+        stop();
+    }
 
-	private static final class SandboxInfo {
-		private final String sandboxPath;
-		private final String projectPath;
-		private final String projectVersion;
-		private final String serverHostAndPort;
-		private final boolean subSandbox;
+    private static void addIgnoredFiles() {
+        String patterns = FileTypeManager.getInstance().getIgnoredFilesList();
 
-		SandboxInfo(String sandboxPath, String serverHostAndPort, String projectPath, String projectVersion) {
-			this.sandboxPath = sandboxPath;
-			this.serverHostAndPort = serverHostAndPort;
-			this.projectPath = projectPath;
-			this.projectVersion = projectVersion;
-			this.subSandbox = false;
+        StringBuffer newPattern = new StringBuffer(patterns);
+        if (patterns.indexOf(MKS_PROJECT_PJ) == -1) {
+            newPattern.append((newPattern.charAt(newPattern.length() - 1) == ';') ? "" : ";").append(MKS_PROJECT_PJ);
+        }
 
-		}
+        final String newPatternString = newPattern.toString();
+        if (!newPatternString.equals(patterns)) {
+            ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                public void run() {
+                    FileTypeManager.getInstance().setIgnoredFilesList(newPatternString);
+                }
+            }
+            );
+        }
+    }
 
-		public SandboxInfo(SandboxInfo parentSandbox, String sandboxPath, String subProjectPath,
-						   String projectVersion) {
-			this.sandboxPath = sandboxPath;
-			this.projectPath = parentSandbox.getServerFolder() + subProjectPath;
-			this.serverHostAndPort = parentSandbox.serverHostAndPort;
-			this.projectVersion = projectVersion;
-			this.subSandbox = true;
-		}
+    @Override
+    protected void handleLine(String line) {
+        try {
+            if (shoudIgnore(line)) {
+                return;
+            }
+            if (line.startsWith("-----")) {
+                // detection of a new update
+                LOGGER.debug("update notification : " + line);
+                updateSandboxList();
+            }
+        } catch (Exception e) {
+            LOGGER.error("error parsing mks synchronizer output [" + line + "], skipping that line  because : " +
+                    e.getMessage(), e);
+        }
+    }
 
-		public String getServerFolder() {
-			return projectPath.substring(0, projectPath.lastIndexOf('/') + 1);
-		}
-	}
+    private void updateSandboxList() {
+        this.sandboxCacheLock.lock();
+        ArrayList<SandboxesCommand.SandboxInfo> oldList;
+        final ArrayList<SandboxListListener> listeners;
+        ArrayList<SandboxesCommand.SandboxInfo> newSandboxList;
+        try {
+            newSandboxList = getNewSandboxList();
+            listeners = this.listeners;
+            oldList = this.currentList;
+            this.currentList = newSandboxList;
+        } finally {
+            this.sandboxCacheLock.unlock();
+        }
 
-	private void fireSandboxAdded(SandboxInfo sandbox) {
-		sandboxCacheLock.lock();
-		try {
-			currentBatch.add(sandbox);
-			for (SandboxListListener listener : listeners) {
-				listener.addSandboxPath(sandbox.sandboxPath, sandbox.serverHostAndPort, sandbox.projectPath,
-						sandbox.projectVersion, sandbox.subSandbox);
-			}
-		} finally {
-			sandboxCacheLock.unlock();
-		}
-	}
+        compareAndFireUpdates(oldList, newSandboxList, listeners);
+    }
 
-	private void fireSandboxReset() {
-		sandboxCacheLock.lock();
-		try {
-			currentBatch.clear();
-			for (SandboxListListener listener : listeners) {
-				listener.clear();
-			}
-		} finally {
-			sandboxCacheLock.unlock();
-		}
-	}
+    /**
+     * compares the 2 (sorted) lists of sandbox and fire updates appropriately to the provided list of listeners
+     *
+     * @param oldList
+     * @param newList
+     * @param listeners
+     */
+    protected void compareAndFireUpdates(ArrayList<SandboxesCommand.SandboxInfo> oldList, ArrayList<SandboxesCommand.SandboxInfo> newList, ArrayList<SandboxListListener> listeners) {
+        int oldIndex = 0;
+        int newIndex = 0;
 
-	private boolean isSubSandbox(String projectPath) {
-		return !(isUnixAbsolutePath(projectPath) || isWindowAbsolutePath(projectPath));
-	}
+        while (oldIndex < oldList.size() && newIndex < newList.size()) {
+            final SandboxesCommand.SandboxInfo oldSandbox = oldList.get(oldIndex);
+            final SandboxesCommand.SandboxInfo newSandbox = newList.get(newIndex);
 
-	private boolean isWindowAbsolutePath(String projectPath) {
-		return projectPath.length() > 1 && projectPath.charAt(1) == ':' && projectPath.charAt(2) == '/';
-	}
+            final int compareCode = oldSandbox.sandboxPath.compareTo(newSandbox.sandboxPath);
+            if (0 == compareCode) {
+                if (!oldSandbox.equals(newSandbox)) {
+                    fireSandboxUpdated(newSandbox, listeners);
+                }
+                oldIndex++;
+                newIndex++;
+            } else if (compareCode < 0) {
+                fireSandboxRemoved(oldSandbox, listeners);
+                oldIndex++;
+            } else {
+                fireSandboxAdded(newSandbox, listeners);
+                newIndex++;
+            }
+        }
 
-	private boolean isUnixAbsolutePath(String projectPath) {
-		return projectPath.charAt(0) == '/';
-	}
+        while (oldIndex < oldList.size()) {
+            fireSandboxRemoved(oldList.get(oldIndex), listeners);
+            oldIndex++;
+        }
+        while (newIndex < newList.size()) {
+            fireSandboxAdded(newList.get(newIndex), listeners);
+            newIndex++;
+        }
+    }
 
-	public String getDescription() {
-		return "sandbox list listener";
-	}
+
+    private void fireSandboxAdded(SandboxesCommand.SandboxInfo sandbox, ArrayList<SandboxListListener> listeners) {
+        for (SandboxListListener listener : listeners) {
+            listener.addSandboxPath(sandbox.sandboxPath, sandbox.serverHostAndPort, sandbox.projectPath,
+                    sandbox.projectVersion, sandbox.subSandbox);
+        }
+    }
+
+    private void fireSandboxRemoved(SandboxesCommand.SandboxInfo sandbox, ArrayList<SandboxListListener> listeners) {
+        for (SandboxListListener listener : listeners) {
+            listener.removeSandboxPath(sandbox.sandboxPath, sandbox.subSandbox);
+        }
+    }
+
+    private void fireSandboxUpdated(SandboxesCommand.SandboxInfo sandbox, ArrayList<SandboxListListener> listeners) {
+        for (SandboxListListener listener : listeners) {
+            listener.updateSandboxPath(sandbox.sandboxPath, sandbox.serverHostAndPort, sandbox.projectPath,
+                    sandbox.projectVersion, sandbox.subSandbox);
+        }
+    }
+
+    public String getDescription() {
+        return "sandbox list listener";
+    }
+
+    protected ArrayList<SandboxesCommand.SandboxInfo> getNewSandboxList() {
+        final SandboxesCommand command = new SandboxesCommand(new ArrayList<VcsException>(),
+                ApplicationManager.getApplication().getComponent(MksConfiguration.class));
+        return command.result;
+    }
 }
