89f0dd85df86921acf09379437c57a3578eb90e5
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2008-12-17 10:26:14 +0000

    switch to IDEA 9540

--- mks-idea-0.7/src/main/java/org/intellij/vcs/mks/MksRollbackEnvironment.java
+++ mks-idea-0.7/src/main/java/org/intellij/vcs/mks/MksRollbackEnvironment.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.ContentRevision;
 import com.intellij.openapi.vcs.rollback.DefaultRollbackEnvironment;
+import com.intellij.openapi.vcs.rollback.RollbackProgressListener;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
@@ -15,55 +16,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MksRollbackEnvironment extends DefaultRollbackEnvironment {
-	private final Logger logger = Logger.getInstance(MksRollbackEnvironment.class.getName());
-	private final MksVcs mksVcs;
+    private final Logger logger = Logger.getInstance(MksRollbackEnvironment.class.getName());
+    private final MksVcs mksVcs;
 
-	public MksRollbackEnvironment(MksVcs mksVcs) {
-		this.mksVcs = mksVcs;
-	}
+    public MksRollbackEnvironment(MksVcs mksVcs) {
+        this.mksVcs = mksVcs;
+    }
 
-	@Override
-	public String getRollbackOperationName() {
-		return "Revert";
-	}
+    @Override
+    public String getRollbackOperationName() {
+        return "Revert";
+    }
 
-	/**
-	 * This is the most common case of rollback: rolling back changes to files which
-	 * have been checked out.
-	 *
-	 * @param changes changes to be rolled back
-	 * @return a list of exceptions
-	 */
-	public List<VcsException> rollbackChanges(List<Change> changes) {
-		// we can use "si revert ${change.getBeforeRevision().getFile()}" this will revert the file to the
-		// project member revision
-		// todo but it could be that the revision we should revert to is not the member revision ?
 
-		final List<VcsException> exceptions = new ArrayList<VcsException>();
+    /**
+     * This is the most common case of rollback: rolling back changes to files which
+     * have been checked out.
+     */
+    public void rollbackChanges(List<Change> changes, List<VcsException> vcsExceptions, @NotNull RollbackProgressListener rollbackProgressListener) {
+        // we can use "si revert ${change.getBeforeRevision().getFile()}" this will revert the file to the
+        // project member revision
+        // todo but it could be that the revision we should revert to is not the member revision ?
 
-		for (Change change : changes) {
-			File beforePath = null;
-			logger.debug("MksRollbackEnvironment.rollbackChanges " + change);
-			ContentRevision beforeRevision = change.getBeforeRevision();
-			if (beforeRevision != null && beforeRevision instanceof MksContentRevision) {
-				// file was not created by change
-				beforePath = beforeRevision.getFile().getIOFile();
-				revert(beforePath, exceptions);
-			}
-			ContentRevision afterRevision = change.getAfterRevision();
-			if (afterRevision != null) {
-				File afterPath = afterRevision.getFile().getIOFile();
-				if (!afterPath.equals(beforePath)) {
-					// todo file has been renamed !!!
+        for (Change change : changes) {
+            File beforePath = null;
+            logger.debug("MksRollbackEnvironment.rollbackChanges " + change);
+            ContentRevision beforeRevision = change.getBeforeRevision();
+            if (beforeRevision != null && beforeRevision instanceof MksContentRevision) {
+                // file was not created by change
+                beforePath = beforeRevision.getFile().getIOFile();
+                revert(beforePath, vcsExceptions);
+            }
+            ContentRevision afterRevision = change.getAfterRevision();
+            if (afterRevision != null) {
+                File afterPath = afterRevision.getFile().getIOFile();
+                if (!afterPath.equals(beforePath)) {
+                    // todo file has been renamed !!!
 /*
                     UnversionedFilesCollector collector = new UnversionedFilesCollector();
                     try {
                         ((SvnChangeProvider) mySvnVcs.getChangeProvider()).getChanges(afterRevision.getFile(), false, collector);
                     }
                     catch (SVNException e) {
-                        exceptions.add(new VcsException(e));
+                        vcsExceptions.add(new VcsException(e));
                     }
-                    checkRevertFile(afterPath, exceptions);
+                    checkRevertFile(afterPath, vcsExceptions);
                     // rolling back a rename should delete the after file
                     if (beforePath != null) {
                         for (VirtualFile f : collector.getUnversionedFiles()) {
@@ -73,77 +70,76 @@ public class MksRollbackEnvironment extends DefaultRollbackEnvironment {
                         FileUtil.delete(afterPath);
                     }
 */
-				}
-			}
-		}
+                }
+            }
+        }
+    }
 
-		return exceptions;
+    /**
+     * reverts the file using "si revert"
+     *
+     * @param file       the IO file to be reverted
+     * @param exceptions holder for any exceptions occuring
+     */
+    private void revert(File file, List<VcsException> exceptions) {
+        logger.debug("MksRollbackEnvironment.revert " + file);
+        // si revert --overwriteChanged
+        /*
+si revert --batch --overwriteUnchanged --overwriteChanged --overwriteDeferred  --restoreTimestamp [(-S sandbox|--sandbox=sandbox)] [(-F file|--selectionFile=file)]
 
-	}
 
-	/**
-	 * reverts the file using "si revert"
-	 *
-	 * @param file	   the IO file to be reverted
-	 * @param exceptions holder for any exceptions occuring
-	 */
-	private void revert(File file, List<VcsException> exceptions) {
-		logger.debug("MksRollbackEnvironment.revert " + file);
-		// todo throw new UnsupportedOperationException("Method revert not yet implemented");
-	}
+--------------------------------------------------------------------------------
 
-	/**
-	 * This is called when the user performs an "undo" that returns a file to a
-	 * state in which it was checked out or last saved. The implementation of this
-	 * method can compare the current state of the file with the base revision and
-	 * undo the checkout if the file is identical. Implementing this method is optional.
-	 *
-	 * @param file the virtual file to be rolledback
-	 */
-	@Override
-	public void rollbackIfUnchanged(@NotNull VirtualFile file) {
-		// todo
-		logger.debug("MksRollbackEnvironment.rollbackIfUnchanged " + file);
-	}
+         */
+        // todo throw new UnsupportedOperationException("Method revert not yet implemented");
+    }
 
-	/**
-	 * This is called for files reported as "locally deleted": the user has deleted
-	 * the file locally but not scheduled it for deletion from VCS. The implementation
-	 * of this method should get the current version of the listed files from the
-	 * VCS. <br/>
-	 * You don't need to implement this method if you never report such files to
-	 * ChangelistBuilder.
-	 *
-	 * @param files
-	 * @return
-	 */
-	public List<VcsException> rollbackMissingFileDeletion(List<FilePath> files) {
+    /**
+     * This is called when the user performs an "undo" that returns a file to a
+     * state in which it was checked out or last saved. The implementation of this
+     * method can compare the current state of the file with the base revision and
+     * undo the checkout if the file is identical. Implementing this method is optional.
+     *
+     * @param file the virtual file to be rolledback
+     */
+    @Override
+    public void rollbackIfUnchanged(@NotNull VirtualFile file) {
+        // todo
+        logger.debug("MksRollbackEnvironment.rollbackIfUnchanged " + file);
+    }
 
-		final List<VcsException> exceptions = new ArrayList<VcsException>();
-		for (FilePath filePath : files) {
-			// todo
-			logger.debug("MksRollbackEnvironment.rollbackMissingFileDeletion " + filePath);
-		}
-		return exceptions;
-	}
+    /**
+     * This is called for files reported as "locally deleted": the user has deleted
+     * the file locally but not scheduled it for deletion from VCS. The implementation
+     * of this method should get the current version of the listed files from the
+     * VCS. <br/>
+     * You don't need to implement this method if you never report such files to
+     * ChangelistBuilder.
+     */
+    public void rollbackMissingFileDeletion(List<FilePath> filePaths, List<VcsException> vcsExceptions, RollbackProgressListener rollbackProgressListener) {
+        for (FilePath filePath : filePaths) {
+            // todo
+            logger.debug("MksRollbackEnvironment.rollbackMissingFileDeletion " + filePath);
+        }
+    }
 
-	/**
-	 * This is called for files reported as "modified without checkout": the user
-	 * has made a file writable but did not checkout it from the VCS.
-	 * <p/>
-	 * You don't need to implement this method if you never report such files to ChangelistBuilder.
-	 *
-	 * @param files
-	 * @return
-	 */
-	@Override
-	public List<VcsException> rollbackModifiedWithoutCheckout(List<VirtualFile> files) {
-		final List<VcsException> exceptions = new ArrayList<VcsException>();
-		for (VirtualFile file : files) {
-			revert(VfsUtil.virtualToIoFile(file), exceptions);
-			// todo
-			logger.debug("MksRollbackEnvironment.rollbackModifiedWithoutCheckout " + file);
-		}
-		return exceptions;
-	}
+    /**
+     * This is called for files reported as "modified without checkout": the user
+     * has made a file writable but did not checkout it from the VCS.
+     * <p/>
+     * You don't need to implement this method if you never report such files to ChangelistBuilder.
+     *
+     * @param files
+     * @return
+     */
+    @Override
+    public void rollbackModifiedWithoutCheckout(List<VirtualFile> files,
+                                                List<VcsException> vcsExceptions,
+                                                RollbackProgressListener rollbackProgressListener) {
+        for (VirtualFile file : files) {
+            revert(VfsUtil.virtualToIoFile(file), vcsExceptions);
+            // todo
+            logger.debug("MksRollbackEnvironment.rollbackModifiedWithoutCheckout " + file);
+        }
+    }
 }
