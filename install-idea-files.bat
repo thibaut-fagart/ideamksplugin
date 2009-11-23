394301874432e249a7a5cdeb9aa79751f2fed2f6
thibaut.fagart <thibaut.fagart@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2009-11-23 14:00:01 +0000

    Updates Issue 79 Labels: Branch-trunk Labels: Branch-IDEA9 Status: Fixed-NotReleased

--- install-idea-files.bat
+++ install-idea-files.bat
@@ -1,6 +1,6 @@
 rem complete your IDEA Home folder here
-set IDEA_HOME=C:\Documents and Settings\A6253567\Progs\JetBrains\IntelliJ IDEA 10781
-set IDEA_VERSION=9.0.10781
+set IDEA_HOME=C:\Documents and Settings\A6253567\Progs\JetBrains\IntelliJ IDEA 92.24
+set IDEA_VERSION=92.24
 set MKS_VERSION=8.3.2392
 set IDEA_GROUP_ID=com.intellij
 
--- src/main/java/org/intellij/vcs/mks/MksDiffProvider.java
+++ src/main/java/org/intellij/vcs/mks/MksDiffProvider.java
@@ -1,5 +1,14 @@
 package org.intellij.vcs.mks;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+
+import javax.swing.SwingUtilities;
+
+import org.intellij.vcs.mks.sicommands.GetRevisionInfo;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vcs.FilePath;
@@ -11,113 +20,105 @@ import com.intellij.openapi.vcs.diff.ItemLatestState;
 import com.intellij.openapi.vcs.history.VcsRevisionNumber;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.intellij.vcs.mks.sicommands.GetRevisionInfo;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
 
 /**
  * @author Thibaut Fagart
  */
 public class MksDiffProvider implements DiffProvider {
-	private final Logger LOGGER = Logger.getInstance(getClass().getName());
-	private final MksVcs mksVcs;
+    private final Logger LOGGER = Logger.getInstance(getClass().getName());
+    private final MksVcs mksVcs;
 
-	public MksDiffProvider(final MksVcs mksVcs) {
-		this.mksVcs = mksVcs;
-	}
+    public MksDiffProvider(final MksVcs mksVcs) {
+        this.mksVcs = mksVcs;
+    }
 
-	@Nullable
-	public VcsRevisionNumber getCurrentRevision(final VirtualFile virtualFile) {
-		ArrayList<VcsException> errors = new ArrayList<VcsException>();
-		GetRevisionInfo command = getRevisionInfo(virtualFile, errors);
-		if (errors.isEmpty()) {
-			return command.getWorkingRev();
-		} else {
-			LOGGER.warn("error occurred org.intellij.vcs.mks.MksDiffProvider.getCurrentRevision");
-			return null;
-		}
-	}
+    @Nullable
+    public VcsRevisionNumber getCurrentRevision(final VirtualFile virtualFile) {
+        ArrayList<VcsException> errors = new ArrayList<VcsException>();
+        GetRevisionInfo command = getRevisionInfo(virtualFile, errors);
+        if (errors.isEmpty()) {
+            return command.getWorkingRev();
+        } else {
+            LOGGER.warn("error occurred org.intellij.vcs.mks.MksDiffProvider.getCurrentRevision");
+            return null;
+        }
+    }
 
-	private GetRevisionInfo getRevisionInfo(@NotNull final VirtualFile virtualFile, final ArrayList<VcsException> errors) {
-		GetRevisionInfo command = new GetRevisionInfo(errors, mksVcs, virtualFile.getPath(),
-				VfsUtil.virtualToIoFile(virtualFile.getParent()));
-		command.execute();
-		if (command.errors.isEmpty()) {
-			return command;
-		} else //noinspection ThrowableResultOfMethodCallIgnored
-			if (errors.size() == 1 && errors.get(0).getMessage().equals(GetRevisionInfo.NOT_A_MEMBER)) {
-				Messages.showMessageDialog("Not (or not any more) a member", "title", Messages.getInformationIcon());
-				return command;
-			} else {
-				LOGGER.warn("error occurred retrieving version info for " + virtualFile.getPresentableName());
-				return command;
-			}
-	}
+    private GetRevisionInfo getRevisionInfo(@NotNull final VirtualFile virtualFile, final ArrayList<VcsException> errors) {
+        GetRevisionInfo command = new GetRevisionInfo(errors, mksVcs, virtualFile.getPath(),
+            VfsUtil.virtualToIoFile(virtualFile.getParent()));
+        command.execute();
+        if (command.errors.isEmpty()) {
+            return command;
+        } else //noinspection ThrowableResultOfMethodCallIgnored
+            if (errors.size() == 1 && errors.get(0).getMessage().equals(GetRevisionInfo.NOT_A_MEMBER)) {
+                Messages.showMessageDialog("Not (or not any more) a member", "title", Messages.getInformationIcon());
+                return command;
+            } else {
+                LOGGER.warn("error occurred retrieving version info for " + virtualFile.getPresentableName());
+                return command;
+            }
+    }
 
-	@Nullable
-	public ItemLatestState getLastRevision(@NotNull final VirtualFile virtualFile) {
-		ArrayList<VcsException> errors = new ArrayList<VcsException>();
-		GetRevisionInfo command = getRevisionInfo(virtualFile, errors);
-		if (errors.isEmpty()) {
-			return new ItemLatestState(command.getMemberRev(), true);
-		} else {
-			LOGGER.warn("error occurred org.intellij.vcs.mks.MksDiffProvider.getLastRevision");
-			return null;
-		}
-	}
+    @Nullable
+    public ItemLatestState getLastRevision(@NotNull final VirtualFile virtualFile) {
+        ArrayList<VcsException> errors = new ArrayList<VcsException>();
+        GetRevisionInfo command = getRevisionInfo(virtualFile, errors);
+        if (errors.isEmpty()) {
+            return new ItemLatestState(command.getMemberRev(), true, false);
+        } else {
+            LOGGER.warn("error occurred org.intellij.vcs.mks.MksDiffProvider.getLastRevision");
+            return null;
+        }
+    }
 
-	@Nullable
-	public ContentRevision createFileContent(final VcsRevisionNumber vcsRevisionNumber, final VirtualFile virtualFile) {
-		if (VcsRevisionNumber.NULL.equals(vcsRevisionNumber)) {
-			if (!SwingUtilities.isEventDispatchThread()) {
-				try {
-					SwingUtilities.invokeAndWait(new Runnable() {
-						public void run() {
-							showRevisionNotControlledErrorDialog();
-						}
-					});
-				} catch (InterruptedException e) {
-					Thread.currentThread().interrupt();
-				} catch (InvocationTargetException e) {
-					LOGGER.error(e.getTargetException());
-				}
-			} else {
-				showRevisionNotControlledErrorDialog();
-			}
-			return null;
-		}
-		return new MksContentRevision(mksVcs,
-				VcsContextFactory.SERVICE.getInstance().createFilePathOn(virtualFile), vcsRevisionNumber);
-	}
+    @Nullable
+    public ContentRevision createFileContent(final VcsRevisionNumber vcsRevisionNumber, final VirtualFile virtualFile) {
+        if (VcsRevisionNumber.NULL.equals(vcsRevisionNumber)) {
+            if (!SwingUtilities.isEventDispatchThread()) {
+                try {
+                    SwingUtilities.invokeAndWait(new Runnable() {
+                        public void run() {
+                            showRevisionNotControlledErrorDialog();
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } catch (InvocationTargetException e) {
+                    LOGGER.error(e.getTargetException());
+                }
+            } else {
+                showRevisionNotControlledErrorDialog();
+            }
+            return null;
+        }
+        return new MksContentRevision(mksVcs,
+            VcsContextFactory.SERVICE.getInstance().createFilePathOn(virtualFile), vcsRevisionNumber);
+    }
 
-	private void showRevisionNotControlledErrorDialog() {
-		Messages.showWarningDialog("This revision is not mks controlled", "Error");
-	}
+    private void showRevisionNotControlledErrorDialog() {
+        Messages.showWarningDialog("This revision is not mks controlled", "Error");
+    }
 
-	@Override
-	@Nullable
-	public ItemLatestState getLastRevision(FilePath filePath) {
-		final VirtualFile file = filePath.getVirtualFile();
-		return (file == null) ? null : getLastRevision(file);
-	}
+    @Override
+    @Nullable
+    public ItemLatestState getLastRevision(FilePath filePath) {
+        final VirtualFile file = filePath.getVirtualFile();
+        return (file == null) ? null : getLastRevision(file);
+    }
 
-	/**
-	 * The method is used to check whether there were any new commits under some root. It can return null -
-	 * than it would mean that there's no information about whether something had changed (result of this method
-	 * invocation is cached and compared with next invocation's result). <br/>
-	 * So, return null.
-	 *
-	 * @param vcsRoot
-	 * @return null if revisions are not repository wide
-	 * @see <a href="http://www.jetbrains.net/devnet/message/5243690#5243690">forum</a>
-	 */
-	@Override
-	@Nullable
-	public VcsRevisionNumber getLatestCommittedRevision(VirtualFile vcsRoot) {
+    /**
+     * The method is used to check whether there were any new commits under some root. It can return null - than it would mean that
+     * there's no information about whether something had changed (result of this method invocation is cached and compared with next
+     * invocation's result). <br/> So, return null.
+     *
+     * @param vcsRoot
+     * @return null if revisions are not repository wide
+     * @see <a href="http://www.jetbrains.net/devnet/message/5243690#5243690">forum</a>
+     */
+    @Override
+    @Nullable
+    public VcsRevisionNumber getLatestCommittedRevision(VirtualFile vcsRoot) {
 		return null;
 	}
 }
