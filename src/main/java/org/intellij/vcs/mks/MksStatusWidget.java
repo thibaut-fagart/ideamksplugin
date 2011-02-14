1b022a883372ab1a5ef0ce72b41e9295c1f05c10
thibaut.fagart@gmail.com <thibaut.fagart@gmail.com@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2011-02-14 12:55:03 +0000

    Update issue 84 Labels: Branch-trunk Status: Fixed-NotReleased

--- src/main/java/org/intellij/vcs/mks/MksStatusWidget.java
+++ src/main/java/org/intellij/vcs/mks/MksStatusWidget.java
@@ -8,60 +8,60 @@ import org.jetbrains.annotations.NotNull;
 import java.awt.event.MouseEvent;
 
 public class MksStatusWidget implements StatusBarWidget {
-    private String currentStatus = "";
-    private static final String MAX = "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789"
-            + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789";
-    private final TextPresentation presentation = new StatusPresentation();
+	private String currentStatus = "";
+	private static final String MAX = "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789"
+			+ "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789";
+	private final TextPresentation presentation = new StatusPresentation();
 
-    public void setText(String message) {
-        currentStatus = message;
-    }
+	public void setText(String message) {
+		currentStatus = message;
+	}
 
-    class StatusPresentation implements TextPresentation {
-        @Override
-        public float getAlignment() {
-            return -1;
-        }
+	class StatusPresentation implements TextPresentation {
+		@Override
+		public float getAlignment() {
+			return -1;
+		}
 
-        @NotNull
-        @Override
-        public String getText() {
-            return MksStatusWidget.this.currentStatus;
-        }
+		@NotNull
+		@Override
+		public String getText() {
+			return MksStatusWidget.this.currentStatus;
+		}
 
-        @NotNull
-        @Override
-        public String getMaxPossibleText() {
-            return MAX;
-        }
+		@NotNull
+		@Override
+		public String getMaxPossibleText() {
+			return MAX;
+		}
 
-        @Override
-        public String getTooltipText() {
-            return null;
-        }
+		@Override
+		public String getTooltipText() {
+			return null;
+		}
 
-        @Override
-        public Consumer<MouseEvent> getClickConsumer() {
-            return null;
-        }
-    }
+		@Override
+		public Consumer<MouseEvent> getClickConsumer() {
+			return null;
+		}
+	}
 
-    @Override
-    public WidgetPresentation getPresentation(@NotNull Type type) {
-        return presentation;
-    }
+	@Override
+	public WidgetPresentation getPresentation(@NotNull PlatformType type) {
+		return presentation;
+	}
 
-    @NotNull
-    @Override
-    public String ID() {
-        return "MKS_Status";
-    }
+	@NotNull
+	@Override
+	public String ID() {
+		return "MKS_Status";
+	}
 
-    @Override
-    public void install(@NotNull StatusBar statusBar) {
-    }
+	@Override
+	public void install(@NotNull StatusBar statusBar) {
+	}
 
-    @Override
-    public void dispose() {
-    }
+	@Override
+	public void dispose() {
+	}
 }
