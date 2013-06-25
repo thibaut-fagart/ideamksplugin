7f03905f44d1ea461f0bdeabb523fa0085c0d445
thibaut.fagart@gmail.com <thibaut.fagart@gmail.com@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2013-06-25 07:52:44 +0000

    Create branch non-native-api

---
+++ branches/non-native-api/src/main/java/org/intellij/vcs/mks/CommandExecutionAdapter.java
@@ -0,0 +1,122 @@
+package org.intellij.vcs.mks;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+final class CommandExecutionAdapter extends AbstractTableModel
+		implements CommandExecutionListener {
+	static class CommandStatistic {
+		final
+		@NotNull
+		String command;
+		long executionCount = 0;
+		long totalExecutionTime = 0;
+
+		CommandStatistic(@NotNull String command) {
+			this.command = command;
+		}
+
+		public boolean equals(@Nullable Object o) {
+			return this == o ||
+					!(o == null || getClass() != o.getClass()) && command
+							.equals(((CommandStatistic) o).command);
+		}
+
+		public int hashCode() {
+			return command.hashCode();
+		}
+
+		private void reset() {
+			executionCount = 0;
+			totalExecutionTime = 0;
+		}
+
+		public long averageInMs() {
+			return (executionCount == 0) ? 0 :
+					(totalExecutionTime / executionCount);
+		}
+
+		public long totalExecutionTimeInS() {
+			return totalExecutionTime / 1000;
+		}
+	}
+
+	final Map<String, CommandStatistic> statisticsByCommand =
+			new HashMap<String, CommandStatistic>();
+	final List<CommandStatistic> rows = new ArrayList<CommandStatistic>();
+	private static final int NAME = 0;
+	private static final int COUNT = 1;
+	private static final int AVERAGE_TIME = 2;
+	private static final int TOTAL_TIME = 3;
+	private static final String[] COLUMN_TITLES =
+			{MksBundle.message("performance_tab.column.title.command"),
+					MksBundle.message("performance_tab.column.title.execution_count"),
+					MksBundle.message("performance_tab.column.title.average.time.ms"),
+					MksBundle.message(
+							"performance_tab.column.title.total.execution.time.s")};
+
+	public int getRowCount() {
+		return rows.size();
+	}
+
+	public int getColumnCount() {
+		return 4;
+	}
+
+	@NotNull
+	public String getColumnName(int column) {
+		return COLUMN_TITLES[column];
+	}
+
+	public synchronized void clear() {
+		for (CommandStatistic statistic : rows) {
+			statistic.reset();
+		}
+		fireTableDataChanged();
+	}
+
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		assert rowIndex >= 0 && rowIndex < rows.size() :
+				"row is out of range : " + rowIndex;
+		final CommandStatistic statistic = rows.get(rowIndex);
+		switch (columnIndex) {
+			case NAME:
+				return statistic.command;
+			case COUNT:
+				return statistic.executionCount;
+			case AVERAGE_TIME:
+				return statistic.averageInMs();
+			case TOTAL_TIME:
+				return statistic.totalExecutionTimeInS();
+			default:
+				throw new IllegalArgumentException(
+						"unknown column index " + columnIndex);
+		}
+	}
+
+	public synchronized void executionCompleted(String command, long duration) {
+		CommandStatistic stat = statisticsByCommand.get(command);
+		boolean dataChanged = false;
+		if (stat == null) {
+			stat = new CommandStatistic(command);
+			statisticsByCommand.put(command, stat);
+			rows.add(stat);
+			dataChanged = true;
+		}
+		stat.totalExecutionTime += duration;
+		stat.executionCount++;
+		if (dataChanged) {
+			fireTableDataChanged();
+		} else {
+			final int row = rows.indexOf(stat);
+			fireTableRowsUpdated(row, row);
+		}
+	}
+}
---
+++ branches/non-native-api/src/main/java/org/intellij/vcs/mks/LoginDialog.java
@@ -0,0 +1,81 @@
+package org.intellij.vcs.mks;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class LoginDialog extends JDialog {
+	private JPanel contentPane;
+	private JButton buttonOK;
+	private JButton buttonCancel;
+	private JTextField login;
+	private JPasswordField password;
+	private boolean wasCanceled = true;
+
+	public LoginDialog(JFrame frame, String hostAndPort) {
+		super(frame, MksBundle.message("mks.reconnect.to.server", hostAndPort));
+		final Point parentLocation = frame.getLocation();
+		final Dimension parentDimension = frame.getSize();
+		final Point target = new Point(
+		parentLocation.x + parentDimension.width / 2,
+				parentLocation.y + parentDimension.height / 2);
+		setLocation(target);
+		setContentPane(contentPane);
+		setModal(true);
+		getRootPane().setDefaultButton(buttonOK);
+
+		buttonOK.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				onOK();
+			}
+		});
+
+		buttonCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				onCancel();
+			}
+		});
+
+// call onCancel() when cross is clicked
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				onCancel();
+			}
+		});
+
+// call onCancel() on ESCAPE
+		contentPane.registerKeyboardAction(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				onCancel();
+			}
+		}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+	}
+
+	private void onOK() {
+// add your code here
+		wasCanceled = false;
+		dispose();
+	}
+
+	private void onCancel() {
+// add your code here if necessary
+		dispose();
+	}
+	public boolean isCanceled() {
+		return wasCanceled;
+	}
+	public static void main(String[] args) {
+		LoginDialog dialog = new LoginDialog(new JFrame(), "toto:7001");
+		dialog.pack();
+		dialog.setVisible(true);
+		System.exit(0);
+	}
+
+	public String getUser() {
+		return this.login.getText();
+	}
+	public String getPassword () {
+		return new String(this.password.getPassword());
+	}
+}
