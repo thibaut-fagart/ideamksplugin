586b80878b125b36da3bcc63f76251f8116be198
thibaut.fagart@gmail.com <thibaut.fagart@gmail.com@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2014-11-10 13:45:14 +0000

    Update   issue 105 Labels: Branch-trunk Status: Fixed-NotReleased

--- trunk/src/main/java/org/intellij/vcs/mks/MksContentRevision.java
+++ trunk/src/main/java/org/intellij/vcs/mks/MksContentRevision.java
@@ -4,7 +4,10 @@ import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.ContentRevision;
 import com.intellij.openapi.vcs.history.VcsRevisionNumber;
+import org.intellij.vcs.mks.model.MksServerInfo;
+import org.intellij.vcs.mks.realtime.MksSandboxInfo;
 import org.intellij.vcs.mks.sicommands.cli.GetContentRevision;
+import org.intellij.vcs.mks.sicommands.cli.SiCLICommand;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,11 +33,35 @@ public class MksContentRevision implements ContentRevision {
 		if (VcsRevisionNumber.NULL.equals(myRevision)) {
 			return null;
 		} else {
-			GetContentRevision getRevisionCommand = new GetContentRevision(new ArrayList<VcsException>(), mksvcs,
-					this.getRevisionNumber(), this.getFile().getPath());
-			getRevisionCommand.execute();
+			GetContentRevision getRevisionCommand = executeGetRevisionCommand();
+			if (getRevisionCommand.foundError()) {
+				VcsException vcsException = getRevisionCommand.errors.get(getRevisionCommand.errors.size() - 1);
+				if (SiCLICommand.UNABLE_TO_RECONNECT_TO_MKS_SERVER.equals(vcsException.getMessage())) {
+					getRevisionCommand = reconnectAndRetry();
+				}
+			}
+			if (getRevisionCommand.foundError()) {
+				throw getRevisionCommand.errors.get(getRevisionCommand.errors.size() - 1);
+			}
 			return getRevisionCommand.getContent();
 		}
+	}
+
+	private GetContentRevision executeGetRevisionCommand() {
+		GetContentRevision getRevisionCommand = new GetContentRevision(new ArrayList<VcsException>(), mksvcs,
+				this.getRevisionNumber(), this.getFile().getPath());
+		getRevisionCommand.execute();
+		return getRevisionCommand;
+	}
+
+	private GetContentRevision reconnectAndRetry() throws VcsException {
+		MksSandboxInfo sandboxInfo = mksvcs.getSandboxCache().getSandboxInfo(myFile.getVirtualFile());
+		if (sandboxInfo == null) {
+			throw new VcsException("unable to find sandbox  for [" + myFile + "]");
+		}
+		MKSAPIHelper.getInstance().reconnect(mksvcs.getProject(), MksServerInfo.fromHostAndPort(sandboxInfo.hostAndPort));
+//						checkNeededServersAreOnlineAndReconnectIfNeeded(sandboxInfos, MKSAPIHelper.getInstance().getMksServers(null, new ArrayList<VcsException>(), mksvcs), mksvcs.getProject());
+		return executeGetRevisionCommand();
 	}
 
 	@NotNull
