7b8e9583232a5150ae2af8fced766ac23ba2b853
thibaut.fagart@gmail.com <thibaut.fagart@gmail.com@e19bf6f1-ed2e-0410-8f0e-bf528b27d39e>
2014-11-13 12:40:56 +0000

    Update   issue 107 Labels: Branch-trunk Status: Fixed-NotReleased Labels: Milestone-Release0.12

--- src/main/java/org/intellij/vcs/mks/sicommands/api/ViewChangePackageAPICommand.java
+++ src/main/java/org/intellij/vcs/mks/sicommands/api/ViewChangePackageAPICommand.java
@@ -11,18 +11,20 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class ViewChangePackageAPICommand extends SiAPICommand {
-	private final MksChangePackage changePackage;
-
+	private final String changePackageId;
 	public ViewChangePackageAPICommand(@NotNull List<VcsException> errors, @NotNull MksCLIConfiguration mksCLIConfiguration, MksChangePackage cp) {
+		this (errors, mksCLIConfiguration, cp.getId());
+	}
+	public ViewChangePackageAPICommand(@NotNull List<VcsException> errors, @NotNull MksCLIConfiguration mksCLIConfiguration, String cpid) {
 		super(errors, "viewcp", mksCLIConfiguration);
-		this.changePackage = cp;
+		this.changePackageId = cpid;
 	}
 
 	@Override
 	public void execute() {
 		long start = System.currentTimeMillis();
 		try {
-			getAPIHelper().getSICommands().siChangePackageView(changePackage.getId());
+			getAPIHelper().getSICommands().siChangePackageView(changePackageId);
 		} catch (APIException e) {
 			errors.add(new VcsException(e));
 		} finally {
